1fac7e9b05347f89e34a5f120a78f8934fd61d07
n7dll <n7dll@invalid>
2022-08-18 12:29:45 +0200

    moar changes

--- ApplyImage.bat
+++ ApplyImage.bat
@@ -1,17 +1,10 @@
 diskpart /s CreatePartitions-UEFI.txt
-
 call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-
 dism /Apply-Image /ImageFile:%1 /Index:1 /ApplyDir:W:\
-
+dism /Image:W:\ /Add-Driver /Driver:%2 /recurse
 W:\Windows\System32\bcdboot W:\Windows /s S:
-
 md R:\Recovery\WindowsRE
-
 xcopy /h W:\Windows\System32\Recovery\Winre.wim R:\Recovery\WindowsRE\
-
 W:\Windows\System32\Reagentc /Setreimage /Path R:\Recovery\WindowsRE /Target W:\Windows
-
 W:\Windows\System32\Reagentc /Info /Target W:\Windows
-
 powershell -c "Get-ProvisionedAppPackage -Path W:\ | Remove-ProvisionedAppPackage -Path W:\"
