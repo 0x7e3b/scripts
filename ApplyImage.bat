c8be883abd6d173ba374f3d93bad4f43c747a23f
n7dll <n7dll@invalid>
2022-08-19 09:45:54 +0200

    added user input capabilities

--- ApplyImage.bat
+++ ApplyImage.bat
@@ -1,13 +1,32 @@
+@echo off
+@if %1.==. echo ERROR: To run this script, add a path to a Windows image file.
+@if %1.==. echo Example: ApplyImage D:\install.wim
+@if %1.==. goto END
+
+:INSTALL
 diskpart /s CreatePartitions-UEFI.txt
-call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-set /p wimfile=Please enter the WIM-File location:
-dism /Get-ImageInfo /ImageFile:%wimfile%
-set /p wimindex=Please enter the edition number:
-dism /Apply-Image /ImageFile:%wimfile% /Index:%wimindex% /ApplyDir:W:\
-dism /Image:W:\ /Add-Driver /Driver:%2 /recurse
+@call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+dism /Get-ImageInfo /ImageFile:%1
+@echo Please enter the Index ID of the Windows Edtion you want to install.
+@set /p wimindex=Index ID:
+dism /Apply-Image /ImageFile:%1 /Index:%wimindex% /ApplyDir:W:\
+@echo Do you want to add drivers?
+@set /p ADDDRIVERS=(Y or N):
+@if %ADDDRIVERS%.==y. set ADDDRIVERS=Y
+@if %ADDDRIVERS%.==Y. GOTO DRIVERS
+@if not %ADDDRIVERS%.==Y GOTO SKIPDRIVERS
+
+:DRIVERS
+@echo Please enter the path to a directory which contains drivers
+@echo in .inf format.
+@set /p DRVPATH=Path:
+dism /Image:W:\ /Add-Driver /Driver:%DRVPATH% /recurse
+
+:SKIPDRIVERS
 W:\Windows\System32\bcdboot W:\Windows /s S:
 md R:\Recovery\WindowsRE
 xcopy /h W:\Windows\System32\Recovery\Winre.wim R:\Recovery\WindowsRE\
 W:\Windows\System32\Reagentc /Setreimage /Path R:\Recovery\WindowsRE /Target W:\Windows
 W:\Windows\System32\Reagentc /Info /Target W:\Windows
-powershell -c "Get-ProvisionedAppPackage -Path W:\ | Remove-ProvisionedAppPackage -Path W:\"
+powershell -c "&{Get-ProvisionedAppPackage -Path W:\ | Remove-ProvisionedAppPackage -Path W:\}"
+:END
