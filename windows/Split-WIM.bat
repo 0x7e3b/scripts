3d3cb23f85c4d49f6eb48e18a183f8d8e2b7c34c
0x7e3b <30737139+0x7e3b@users.noreply.github.com>
2023-05-06 18:06:59 +0200

    Removed unnecessary scripts and added new ones

--- windows/Split-WIM.bat
+++
@@ -1,4 +0,0 @@
-set SRC=REPLACE_WITH_SOURCE_DRIVE_LETTER
-set DST=REPLACE_WITH_DESTINATION_DRIVE_LETTER
-
-cmd /c "DISM /Split-Image /ImageFile:"%SRC%:\sources\install.wim" /SWMFile:"%DST%:\sources\install.swm" /FileSize:3800"
